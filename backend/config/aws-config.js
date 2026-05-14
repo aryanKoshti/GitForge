96063fd3c7e7e9015193594b867f979b245b8adf
Aryan Koshti <aryankosti120@gmail.com>
2026-05-14 15:43:38 +0530

    added push command logic

--- backend/config/aws-config.js
+++ backend/config/aws-config.js
@@ -1,12 +1,13 @@
-const AWS = require('aws-sdk');
+const { S3Client } = require("@aws-sdk/client-s3");
 
-AWS.config.update({
-    // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: "ap-south-1"
+const s3 = new S3Client({
+    region: "ap-southeast-2",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
-const s3 = new AWS.S3();
-const S3_BUCKET = "test-bucket-code-1234";
+const S3_BUCKET = "sampleakbucket";
 
 module.exports = { s3, S3_BUCKET };
