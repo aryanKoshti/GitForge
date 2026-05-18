23788ec5a3796f4c47000c87a47e4ca826f2b527
Aryan Koshti <aryankosti120@gmail.com>
2026-05-18 14:37:04 +0530

    created user, repo & issue model

---
+++ backend/models/repoModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const RepositorySchema = new Schema({
+    repoName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    discription: {
+        type: String,
+    },
+    content: [
+        {
+            type: String,
+
+        }
+    ],
+    visibility: {
+        type: Boolean,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    issues: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Issue"
+        }
+    ]
+});
+
+const Repository = mongoose.model("Repository", RepositorySchema);
+
+export default Repository;
---
+++ backend/models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    repositories: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "Repository",
+        },
+    ],
+
+    followesUsers: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    starRepo: [
+        {
+            default: [],
+            type: Schema.Types.ObjectId,
+            ref: "Repository",
+        },
+    ],
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
