414949069d55b054675bb7da472831a4d8be6b62
Aryan Koshti <aryankosti120@gmail.com>
2026-05-19 09:47:55 +0530

    added repo controller

---
+++ backend/controllers/repoController.js
@@ -0,0 +1,44 @@
+
+
+const createRepository = (req, res) => {
+    res.send("Repository created!")
+}
+
+const getAllRepository = (req, res) => {
+    res.send("All repositories fetched!")
+};
+
+const fetchRepositoryById = (req, res) => {
+    res.send("Repository Details Fetched!")
+};
+
+const fetchRepositoryByName = (req, res) => {
+    res.send("Repository Details Fetched!")
+};
+
+const fetchRepositoriesForCurrntUser = (req, res) => {
+    res.send("Repositories for logged in user Fetched!")
+};
+
+const updateRepositoryById = (req, res) => {
+    res.send("Repository updated!")
+}
+
+const toggleVisibliltyById = (req, res) => {
+    res.send("Repository toggled")
+}
+
+const deleteRepositoryById = (req, res) => {
+    res.send("Repository deleted!")
+}
+
+module.exports = {
+    createRepository,
+    getAllRepository,
+    fetchRepositoryById,
+    fetchRepositoryByName,
+    fetchRepositoryForCurrntUser,
+    updateRepositoryById,
+    toggleVisibliltyById,
+    deleteRepositoryById
+}
